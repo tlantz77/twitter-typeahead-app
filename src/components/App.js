4dabc9c0d22673ab6387e15045a2ce33961c8ef1
Tim Lantz <tlantz@gmail.com>
2020-11-25 13:26:26 -0600

    initial commit w/ basic file re-org

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles.css';
+
+import Modal from './Modal';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Modal/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import '../styles.css';
+
+const Modal = () => {
+  return ( 
+    <div className='modal-wrapper'>
+    </div> 
+  );
+}
+ 
+export default Modal;
